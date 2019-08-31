7242064689ae68709c2ab4bca0809f1f242d6669
Unknown <alkomoon2012@gmail.com>
2019-08-31 23:10:21 +0300

    Split to base and derived trees

--- Randomized BST/rbst/RBST.h
+++ Randomized BST/rbst/RBST.h
@@ -1,84 +1,26 @@
 #pragma once
 
-#include <memory>
-#include <random>
-#include <utility>
-#include <assert.h>
-#include <iostream>
-#include <iterator>
-#include <string>
+#include <AbstractBST.h>
 
 template <typename K, typename V>
-class RBST {
-	struct Node;
-
+class RBST : public AbstractBST<K, V, std::forward_iterator_tag> {
 public:
-	class NodeIterator;
-
-	using iterator = NodeIterator;
-	using const_iterator = const NodeIterator;
-	using KVPair = std::pair<K, V>;
+	using AbstractBaseTree = AbstractBST<K, V, std::forward_iterator_tag>;
+	using AbstractBaseTree::find;
 
 	RBST() = default;
 
-	bool contains(const K& key) const;
-
-	iterator find(const K& key) const;
-
-	void insert(const K& key, const V& value);
+	void insert(const K& key, const V& value) override;
 	
-	bool remove(const K& key);
-
-	void clear();
-
-	size_t size() const;
+	bool remove(const K& key) override;
 
 	void printTree() const;
 
-	iterator begin() const;
-	const_iterator cbegin() const;
-
-	iterator end() const;
-	const_iterator cend() const;
-
-	class NodeIterator final : public std::iterator<std::bidirectional_iterator_tag, typename Node::Ptr> {
-	public:
-		NodeIterator() = default;
-		NodeIterator(typename Node::Ptr ptr);
-		NodeIterator(const NodeIterator& other);
-
-		NodeIterator& operator++();
-		NodeIterator operator++(int);
-
-		bool operator==(const NodeIterator& other) const;
-		bool operator!=(const NodeIterator& other) const;
-
-		KVPair& operator*();
-		const KVPair& operator*() const;
-
-		KVPair* operator->();
-		const KVPair* operator->() const;
-
-		operator bool() const;
-
-	private:
-		typename Node::Ptr m_item;
-	};
-
 private:
-	struct Node final {
-		using Ptr = std::shared_ptr<Node>;
-		using WPtr = std::weak_ptr<Node>;
+	struct Node final : public AbstractBaseTree::AbstractNode {
+		Node(const typename AbstractBaseTree::KVPair& keyValue);
 
-		Node(const KVPair& keyValue);
-
-		Ptr next() const;
-
-		KVPair m_keyValue;
-		size_t m_size{ 1 };
-		WPtr m_parent;
-		Ptr m_left;
-		Ptr m_right;
+		typename AbstractBaseTree::AbstractNode::Ptr next() const override;
 	};
 
 	using NodePtr = typename Node::Ptr;
@@ -86,40 +28,25 @@ private:
 	void printBinaryTree(const std::string& prefix, const typename Node::Ptr& node, bool isLeft) const;
 
 	size_t safeGetSize(const NodePtr& node) const;
-	void fixHeight(NodePtr& node);
+	void fixSize(NodePtr& node);
 
-	NodePtr find(const NodePtr& node, const K& key) const;
+	NodePtr find(const NodePtr& node, const K& key) const override;
 
-	NodePtr insert(NodePtr& node, const KVPair& keyValue);
-	NodePtr insertRoot(NodePtr& node, const KVPair& keyValue);
+	NodePtr insert(NodePtr& node, const typename AbstractBaseTree::KVPair& keyValue) override;
+	NodePtr insertRoot(NodePtr& node, const typename AbstractBaseTree::KVPair& keyValue);
 
 	NodePtr rotateRight(NodePtr& node);
 	NodePtr rotateLeft(NodePtr& node);
 
 	NodePtr join(NodePtr& p, NodePtr& q);
 
-	NodePtr remove(NodePtr& p, const K& key);
-
-	NodePtr m_rootNode;
-	size_t m_size{ 0 };
+	NodePtr remove(NodePtr& p, const K& key) override;
 };
 
 template <typename K, typename V>
-bool RBST<K, V>::contains(const K& key) const {
-	return find(m_rootNode, key) != nullptr;
-}
-
-template <typename K, typename V>
 void RBST<K, V>::insert(const K& key, const V& value) {
-	m_rootNode = insert(m_rootNode, std::make_pair(key, value));
-	++m_size;
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::iterator RBST<K, V>::find(const K& key) const {
-	auto ptr = find(m_rootNode, key);
-
-	return iterator(ptr);
+	this->m_rootNode = insert(this->m_rootNode, std::make_pair(key, value));
+	++this->m_size;
 }
 
 template <typename K, typename V>
@@ -128,139 +55,27 @@ bool RBST<K, V>::remove(const K& key) {
 		return false;
 	}
 
-	m_rootNode = remove(m_rootNode, key);
-	--m_size;
+	this->m_rootNode = remove(this->m_rootNode, key);
+	--this->m_size;
 
 	return true;
 }
 
 template <typename K, typename V>
-void RBST<K, V>::clear() {
-	m_rootNode.reset();
-	m_size = 0;
-}
-
-template <typename K, typename V>
-size_t RBST<K, V>::size() const {
-	return m_size;
-}
-
-template <typename K, typename V>
 void RBST<K, V>::printTree() const {
-	printBinaryTree("", m_rootNode, false);
+	printBinaryTree("", this->m_rootNode, false);
 }
 
 template <typename K, typename V>
-typename RBST<K, V>::iterator RBST<K, V>::begin() const {
-	auto node = m_rootNode;
-
-	if (!node) {
-		return iterator();
-	}
-
-	while (true) {
-		if (node->m_left) {
-			node = node->m_left;
-		} else if (node->m_right) {
-			node = node->m_right;
-		} else {
-			break;
-		}
-	}
-
-	return iterator(node);
+RBST<K, V>::Node::Node(const typename AbstractBaseTree::KVPair& keyValue) :
+    AbstractBaseTree::AbstractNode(keyValue)
+{
 }
 
 template <typename K, typename V>
-const typename RBST<K, V>::iterator RBST<K, V>::cbegin() const {
-	return begin();
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::iterator RBST<K, V>::end() const {
-	return iterator();
-}
-
-template <typename K, typename V>
-const typename RBST<K, V>::iterator RBST<K, V>::cend() const {
-	return const_iterator();
-}
-
-template <typename K, typename V>
-RBST<K, V>::NodeIterator::NodeIterator(NodePtr ptr) {
-	m_item = ptr;
-}
-
-template <typename K, typename V>
-RBST<K, V>::NodeIterator::NodeIterator(const typename RBST<K, V>::NodeIterator& other) {
-	*this = other;
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::NodeIterator& RBST<K, V>::NodeIterator::operator++() {
-	m_item = m_item->next();
-	return *this;
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::NodeIterator RBST<K, V>::NodeIterator::operator++(int) {
-	auto tmp = *this;
-	operator++();
-	return tmp;
-}
-
-template <typename K, typename V>
-bool RBST<K, V>::NodeIterator::operator==(const NodeIterator& other) const {
-	if (!m_item && !other.m_item) {
-		return true;
-	}
-
-	if (!other.m_item || !m_item) {
-		return false;
-	}
-
-	return m_item->m_keyValue == other.m_item->m_keyValue;
-}
-
-template <typename K, typename V>
-bool RBST<K, V>::NodeIterator::operator!=(const NodeIterator& other) const {
-	return !(*this == other);
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::KVPair& RBST<K, V>::NodeIterator::operator*() {
-	return m_item->m_keyValue;
-}
-
-template <typename K, typename V>
-const typename RBST<K, V>::KVPair& RBST<K, V>::NodeIterator::operator*() const {
-	return m_item->m_keyValue;
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::KVPair* RBST<K, V>::NodeIterator::operator->() {
-	return &m_item->m_keyValue;
-}
-
-template <typename K, typename V>
-const typename RBST<K, V>::KVPair* RBST<K, V>::NodeIterator::operator->() const {
-	return &m_item->m_keyValue;
-}
-
-template <typename K, typename V>
-RBST<K, V>::NodeIterator::operator bool() const {
-	return m_item != nullptr;
-}
-
-template <typename K, typename V>
-RBST<K, V>::Node::Node(const KVPair& keyValue) {
-	m_keyValue = keyValue;
-}
-
-template <typename K, typename V>
-typename RBST<K, V>::Node::Ptr RBST<K, V>::Node::next() const {
-	Node::Ptr ptr;
-	auto strongParent = m_parent.lock();
+typename RBST<K, V>::AbstractBaseTree::AbstractNode::Ptr RBST<K, V>::Node::next() const {
+	typename AbstractBaseTree::AbstractNode::Ptr ptr;
+	auto strongParent = this->m_parent.lock();
 
 	if (!strongParent) {
 		return ptr;
@@ -317,7 +132,7 @@ size_t RBST<K, V>::safeGetSize(const typename RBST<K, V>::Node::Ptr& node) const
 }
 
 template <typename K, typename V>
-void RBST<K, V>::fixHeight(typename RBST<K, V>::Node::Ptr& node) {
+void RBST<K, V>::fixSize(typename RBST<K, V>::Node::Ptr& node) {
 	if (node) {
 		node->m_size = safeGetSize(node->m_left) + safeGetSize(node->m_right) + 1;
 	}
@@ -337,7 +152,7 @@ typename RBST<K, V>::Node::Ptr RBST<K, V>::find(const NodePtr& node, const K& ke
 }
 
 template <typename K, typename V>
-typename RBST<K, V>::Node::Ptr RBST<K, V>::insert(NodePtr& node, const KVPair& keyValue) {
+typename RBST<K, V>::Node::Ptr RBST<K, V>::insert(NodePtr& node, const typename AbstractBaseTree::KVPair& keyValue) {
 	if (!node) {
 		return std::make_shared<RBST<K, V>::Node>(keyValue);
 	}
@@ -355,13 +170,13 @@ typename RBST<K, V>::Node::Ptr RBST<K, V>::insert(NodePtr& node, const KVPair& k
 		node->m_right->m_parent = node;
 	}
 
-	fixHeight(node);
+	fixSize(node);
 
 	return node;
 }
 
 template <typename K, typename V>
-typename RBST<K, V>::Node::Ptr RBST<K, V>::insertRoot(NodePtr& node, const KVPair& keyValue) {
+typename RBST<K, V>::Node::Ptr RBST<K, V>::insertRoot(NodePtr& node, const typename AbstractBaseTree::KVPair& keyValue) {
 	if (!node) {
 		return std::make_shared<RBST<K, V>::Node>(keyValue);
 	}
@@ -394,7 +209,7 @@ typename RBST<K, V>::Node::Ptr RBST<K, V>::rotateRight(NodePtr& node) {
 	node->m_parent = q;
 	q->m_size = node->m_size;
 
-	fixHeight(node);
+	fixSize(node);
 
 	return q;
 }
@@ -416,7 +231,7 @@ typename RBST<K, V>::Node::Ptr RBST<K, V>::rotateLeft(NodePtr& node) {
 	node->m_parent = p;
 	p->m_size = node->m_size;
 
-	fixHeight(node);
+	fixSize(node);
 
 	return p;
 }
@@ -435,12 +250,12 @@ typename RBST<K, V>::Node::Ptr RBST<K, V>::join(NodePtr& p, NodePtr& q) {
 	if ((rndDev() % (p->m_size + q->m_size)) < p->m_size) {
 		p->m_right = join(p->m_right, q);
 		p->m_right->m_parent = p;
-		fixHeight(p);
+		fixSize(p);
 		return p;
 	} else {
 		q->m_left = join(p, q->m_left);
 		q->m_left->m_parent = q;
-		fixHeight(q);
+		fixSize(q);
 		return q;
 	}
 }
